a424e0493e576a8ca2a38c61f55183ce29e8638b
AbdulrhmanSaad <abdulrhmansaad78@gmail.com>
2026-03-11 14:16:57 +0200

    update authintcation in singlR Client

--- SinglR Client/Program.cs
+++ SinglR Client/Program.cs
@@ -9,19 +9,16 @@ var email=Console.ReadLine();
 Console.WriteLine("Enter Password:");
 var pass=Console.ReadLine();
 
-var loginRequest = new LoginUserCommand
+if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(pass))
 {
-    Email = email,
-    Password = pass
-};
+    Console.WriteLine("Email and Password cannot be empty.");
+    return;
+}
+var token = await Authenticate(email, pass, apiClient);
 
-var loginResponse = await apiClient.LoginUserAsync(loginRequest);
-
-string token = loginResponse.AccessToken;
-
-if(string.IsNullOrEmpty(token))
+if (string.IsNullOrEmpty(token))
 {
-    Console.WriteLine("Login failed.");
+    Console.WriteLine("Authentication failed. Exiting.");
     return;
 }
 
@@ -33,6 +30,7 @@ var connection = new HubConnectionBuilder()
     })
     .WithAutomaticReconnect()
     .Build();
+
 
 connection.On<string,string>("ReceiveMessage", (message,action) =>
 {
@@ -49,3 +47,48 @@ Console.WriteLine("Connected.");
 await connection.InvokeAsync("SendMessage","Message","action");
 
 Console.ReadLine();
+
+
+
+
+
+
+
+static async Task<string?> Authenticate(string email,string pass,Client apiClient)
+{
+    string token=null;
+    var loginRequest = new LoginUserCommand
+    {
+        Email = email,
+        Password = pass
+    };
+    try
+    {
+        var loginResponse = await apiClient.LoginUserAsync(loginRequest);
+
+        token = loginResponse.AccessToken;
+
+        if (!string.IsNullOrEmpty(token))
+        {
+            Console.WriteLine("Login successful.");
+            return token;
+        }
+    }
+    catch (ApiException ex)
+    {
+        if (ex.StatusCode == 404)
+        {
+            Console.WriteLine("Invalid Email Or Password");
+        }
+        else
+        {
+            Console.WriteLine($"API Error: {ex.StatusCode}");
+            Console.WriteLine(ex.Response);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Unexpected error: {ex.Message}");
+    }
+    return null;
+} 
